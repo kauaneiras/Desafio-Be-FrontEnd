3e8ec060ef9a739b2ac301187a870c1fc4352769
Kauan T. Eiras <43351064+kauaneiras@users.noreply.github.com>
2024-03-02 18:50:30 -0300

    feat: navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+import BeLogo from "../assets/images/BeLogo.png";
+
+const Navbar: React.FC = () => {
+  return (
+    <Nav>
+        <Logo src={BeLogo} alt="Be Logo" />
+    </Nav>
+  );
+};
+
+export default Navbar;
+
+const Nav = styled.nav`
+    top: 0;
+    position: fixed;
+    height: 60px;
+    width: 100vw;
+    background-color: #fff;
+    box-shadow: 0px 2px 8px 0px #00000033;
+    display: flex;
+    align-items: center;
+`;
+
+const Logo = styled.img`
+    width: 44px;
+    height: 35px; 
+    margin-left: 32px;
+    padding: 10px;
+    gap: 10px;
+`;
--- src/pages/employer-page.tsx
+++ src/pages/employer-page.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 import { Container } from '../styles/base.tsx';
 import { getData, employees } from '../services/connection.tsx';
+import Navbar from '../components/navbar.tsx';
 
 interface Employee {
   id: string;
@@ -29,21 +31,77 @@ const EmployerPage: React.FC = () => {
   }, []);
 
   return (
-    <Container>
-      <h1>Employer Page</h1>
-      <ul>
-        {data.map((employee) => (
-          <li key={employee.id}>
-            <img src={employee.image} alt={employee.name} />
-            <p>Name: {employee.name}</p>
-            <p>Job: {employee.job}</p>
-            <p>Admission Date: {employee.admission_date}</p>
-            <p>Phone: {employee.phone}</p>
-          </li>
-        ))}
-      </ul>
-    </Container>
+    <StyledContainer>
+      <Navbar />
+      <StyledContent>
+        <StyledList>
+          {data.map((employee) => (
+            <StyledListItem key={employee.id}>
+                <ContainerImg>
+                  <StyledImage src={employee.image} alt={employee.name} />
+                </ContainerImg>
+                <Text>{employee.name}</Text>
+                <Text>{employee.job}</Text>
+                <Text>{employee.admission_date}</Text>
+                <Text>{employee.phone}</Text>
+            </StyledListItem>
+          ))}
+        </StyledList>
+      </StyledContent>
+    </StyledContainer>
   );
 };
+
+const StyledContainer = styled(Container)`
+
+`;
+
+const StyledContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+  width: 95%;
+  height: 70%;
+`;
+
+const StyledList = styled.ul`
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  
+`;
+
+const StyledListItem = styled.li`
+  display: flex;
+  justify-content: flex-start;
+  box-shadow: 0px 1px 2px 0px #00000033;
+`;
+
+const StyledImage = styled.img`
+  min-width: 34px;
+  min-height: 34px;
+  max-width: 40px;
+  max-height: 40px;
+  border-radius: 50%;
+`;
+
+const Text = styled.p`
+  color: black;
+  width: 22.5%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const ContainerImg = styled.div`
+  width: 10%;
+  min-height: 49px;
+  max-height: 55px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default EmployerPage;
